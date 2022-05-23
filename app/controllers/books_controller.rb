4573b798d75d53535f5532edb01acb154a736c8d
EC2 Default User <ec2-user@ip-172-31-25-66.ec2.internal>
2022-05-23 10:05:01 +0000

    [Finish] 4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,13 +4,15 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@book.id), notice: 'You have created book successfully.'
     else
+      @books = Book.all
       render :index
     end
   end
 
   def index
+    @book = Book.new
     @books = Book.all
   end
 
@@ -22,12 +24,17 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user == current_user
+      render :edit
+    else
+      redirect_to books_path
+    end
   end
 
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@book.id), notice: 'You have updated book successfully.'
     else
       render :edit
     end
@@ -42,7 +49,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.permit(:title, :body, :profile_image)
+    params.require(:book).permit(:title, :body)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,26 +2,28 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    @book = Book.new
   end
 
   def show
     @user = User.find(params[:id])
+    @book = Book.new
     @books = @user.books
   end
 
   def edit
     @user = User.find(params[:id])
     if @user == current_user
-      render "edit"
+      render :edit
     else
-      redirect_to user_path
+      redirect_to user_path(current_user.id)
     end
   end
 
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to user_path
+      redirect_to user_path, notice: 'You have updated user successfully.'
     else
       render :edit
     end
@@ -30,7 +32,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 
 end
